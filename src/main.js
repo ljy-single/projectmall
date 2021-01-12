fa9fb9221635dfa7dcacda141d0819cc774b52ba
黑虎 <ljy170702109@163.com>
2021-01-12 11:20:46 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 // 解决移动端300ms的延迟
 import FastClick from 'fastclick'
+FastClick.attach(document.body);
 // 图片懒加载
 import VueLazyload from 'vue-lazyload'
 Vue.use(VueLazyload, {
@@ -14,8 +15,8 @@ Vue.use(VueLazyload, {
 import toast from 'components/common/toast'
 // 安装toast插件，会调用toast中的install方法
 Vue.use(toast)
+
 Vue.config.productionTip = false
-FastClick.attach(document.body);
 // 添加事件总线
 Vue.prototype.$bus = new Vue()
 new Vue({
